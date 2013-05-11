9bd799c5e3eb70bf08e7d92aeab6c588fd73fd1b
Kevin Harwood <kevin.harwood@mutualmobile.com>
2013-05-11 16:16:34 -0500

    Added some example tests

--- Tests/AFHTTPRequestOperationTest.m
+++ Tests/AFHTTPRequestOperationTest.m
@@ -15,6 +15,7 @@
 
 - (void)testThatOperationInvokesSuccessCompletionBlockWithResponseObjectOnSuccess
 {
+    [Expecta setAsynchronousTestTimeout:30.0];
     __block id blockResponseObject = nil;
     NSURLRequest *request = [NSURLRequest requestWithURL:[NSURL URLWithString:@"/get" relativeToURL:AFNetworkingTestsBaseURL()]];
     AFHTTPRequestOperation *operation = [[AFHTTPRequestOperation alloc] initWithRequest:request];
@@ -29,7 +30,7 @@
 - (void)testThatOperationInvokesFailureCompletionBlockWithErrorOnFailure
 {
     __block NSError *blockError = nil;
-    NSURLRequest *request = [NSURLRequest requestWithURL:[NSURL URLWithString:@"/404" relativeToURL:AFNetworkingTestsBaseURL()]];
+    NSURLRequest *request = [NSURLRequest requestWithURL:[NSURL URLWithString:@"/status/404" relativeToURL:AFNetworkingTestsBaseURL()]];
     AFHTTPRequestOperation *operation = [[AFHTTPRequestOperation alloc] initWithRequest:request];
     [operation setCompletionBlockWithSuccess:nil failure:^(AFHTTPRequestOperation *operation, NSError *error) {
         blockError = error;
@@ -37,6 +38,66 @@
     [operation start];
     expect([operation isFinished]).will.beTruthy();
     expect(blockError).willNot.beNil();
+}
+
+- (void)testThat500StatusCodeInvokesFailureCompletionBlockWithErrorOnFailure{
+    __block NSError *blockError = nil;
+    NSURLRequest *request = [NSURLRequest requestWithURL:[NSURL URLWithString:@"/status/500" relativeToURL:AFNetworkingTestsBaseURL()]];
+    AFHTTPRequestOperation *operation = [[AFHTTPRequestOperation alloc] initWithRequest:request];
+    [operation setCompletionBlockWithSuccess:nil failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+        blockError = error;
+    }];
+    [operation start];
+    expect([operation isFinished]).will.beTruthy();
+    expect(blockError).willNot.beNil();
+}
+
+- (void)testThat200StatusCodeInvokesSuccessCompletionBlockWithResponseObjectOnSuccess{
+    __block BOOL success;
+    NSURLRequest *request = [NSURLRequest requestWithURL:[NSURL URLWithString:@"/status/200" relativeToURL:AFNetworkingTestsBaseURL()]];
+    AFHTTPRequestOperation *operation = [[AFHTTPRequestOperation alloc] initWithRequest:request];
+    [operation setCompletionBlockWithSuccess:^(AFHTTPRequestOperation *operation, id responseObject) {
+        success = YES;
+    } failure:nil];
+    [operation start];
+    expect([operation isFinished]).will.beTruthy();
+    expect(success).will.beTruthy();
+}
+
+- (void)testThatRedirectBlockIsCalledWhen302IsEncountered{
+    __block BOOL success;
+    NSURLRequest *request = [NSURLRequest requestWithURL:[NSURL URLWithString:@"/redirect/1" relativeToURL:AFNetworkingTestsBaseURL()]];
+    AFHTTPRequestOperation *operation = [[AFHTTPRequestOperation alloc] initWithRequest:request];
+    [operation setCompletionBlockWithSuccess:nil
+                                     failure:nil];
+    [operation
+     setRedirectResponseBlock:^NSURLRequest *(NSURLConnection *connection, NSURLRequest *request, NSURLResponse *redirectResponse) {
+         if(redirectResponse){
+             success = YES;
+         }
+         return request;
+     }];
+    [operation start];
+    expect([operation isFinished]).will.beTruthy();
+    expect(success).will.beTruthy();
+}
+
+- (void)testThatRedirectBlockIsCalledMultipleTimesWhen302IsEncountered{
+    __block NSInteger numberOfRedirects = 0;
+    NSURLRequest *request = [NSURLRequest requestWithURL:[NSURL URLWithString:@"/redirect/5" relativeToURL:AFNetworkingTestsBaseURL()]];
+    AFHTTPRequestOperation *operation = [[AFHTTPRequestOperation alloc] initWithRequest:request];
+    [operation setCompletionBlockWithSuccess:nil
+                                     failure:nil];
+    [operation
+     setRedirectResponseBlock:^NSURLRequest *(NSURLConnection *connection, NSURLRequest *request, NSURLResponse *redirectResponse) {
+         if(redirectResponse){
+             numberOfRedirects++;
+         }
+         return request;
+     }];
+    [operation start];
+    expect([operation isFinished]).will.beTruthy();
+    expect(numberOfRedirects).will.equal(5);
 }
 
 @end
